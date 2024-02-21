fb2742913e330038851ff381ffebc8af85f61c17
Sebastian Krøger Holmen <79469928+Horseborn@users.noreply.github.com>
2024-02-21 21:00:38 +0100

    First commit

---
+++ ArbeidskravExercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace ArbeidskravExercise;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string vowels = "aeiouAEIOU";
+        string words = "gbmnkkkl";
+        StringBuilder sBuilder = new StringBuilder();
+        
+        foreach (var letter in words)
+        {
+            if (char.IsLetter(letter))
+            {
+
+
+                if (!vowels.Contains(letter) && !Char.IsWhiteSpace(letter))
+                {
+                    sBuilder.Append(letter);
+                    sBuilder.Append("o");
+                    sBuilder.Append(letter);
+                }
+                else
+                {
+                    sBuilder.Append(letter);
+                }
+            }
+            else
+            {
+                sBuilder.Append(letter);
+            }
+        }
+
+        string translatedString = sBuilder.ToString();
+        Console.WriteLine(translatedString);
+
+
+
+    }
+}
